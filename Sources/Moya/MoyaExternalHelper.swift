f4161249dd3b121630e459bf40816350f20e6177
Andrei-Iulian Sadagurschi <iuliankay@yahoo.com>
2020-02-10 11:25:01 +0200

    - added optional timeoutIntervalForRequest on MoyaExternalHelper

--- Sources/Moya/MoyaExternalHelper.swift
+++ Sources/Moya/MoyaExternalHelper.swift
@@ -12,10 +12,14 @@ public struct MoyaExternalHelper {
     public static var externalLogger: ((_ configuration: URLSessionConfiguration) -> ())?
     public static var requestRetrier: RequestRetrier?
     public static var requestAdapter: RequestAdapter?
+    public static var timeoutIntervalForRequest: TimeInterval?
     
     static let manager: Manager = {
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = Manager.defaultHTTPHeaders
+        if let timeoutIntervalForRequest = MoyaExternalHelper.timeoutIntervalForRequest {
+            configuration.timeoutIntervalForRequest = timeoutIntervalForRequest
+        }
         
         MoyaExternalHelper.externalLogger?(configuration)
         
